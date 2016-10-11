d034fa88c9a29d3d962452018fce7933e365710c
Trevor Fenn <sgtpepper43@gmail.com>
2016-10-11 15:02:20 -0600

    Add more docs and doc tests

--- lib/caramelizer.ex
+++ lib/caramelizer.ex
@@ -3,6 +3,50 @@ defmodule Caramelize do
   An Elixir library for converting the keys of snake_case maps to camelCase
   """
 
+  @doc """
+  Camelizes a variety of data structures
+
+  * atom -> camelizes the atom
+  * string -> camelizes the string
+  * nested map -> camelize the nested map keys
+  * list of maps -> camelize the maps
+  * map -> camelize the keys
+  * DateTime -> pass it straight through for easier serialization
+
+  ## Examples
+
+      iex(1)> Caramelize.camelize("cool_string")
+      "coolString"
+
+      iex(2)> Caramelize.camelize(:cool_string)
+      "coolString"
+
+      iex(3)> Caramelize.camelize(%{cool_atom: "foo"})
+      %{"coolAtom" => "foo"}
+
+      iex(4)> Caramelize.camelize(%{"cool_string" => "bar"})
+      %{"coolString" => "bar"}
+
+      iex(5)> Caramelize.camelize(%{cool_atom: %{another_cool_atom: "foo"}})
+      %{"coolAtom" => %{"anotherCoolAtom" => "foo"}}
+
+      iex(6)> Caramelize.camelize(%{"cool_string" => %{
+      ...>      "another_cool_string" => "bar"}
+      ...>    })
+      %{"coolString" => %{"anotherCoolString" => "bar"}}
+
+      iex(7)> Caramelize.camelize([%{cool_key: "foo"}])
+      [%{"coolKey" => "foo"}]
+
+      iex(8)> Caramelize.camelize(%{list_key: [%{foo: 12, bar: 13}]})
+      %{"listKey" => [%{"bar" => 13, "foo" => 12}]}
+
+      iex(9)> Caramelize.camelize(DateTime.from_unix!(1476219081))
+      %DateTime{calendar: Calendar.ISO, day: 11, hour: 20, microsecond: {0, 0},
+       minute: 51, month: 10, second: 21, std_offset: 0, time_zone: "Etc/UTC",
+       utc_offset: 0, year: 2016, zone_abbr: "UTC"}
+  """
+
   # Camelize atoms in a map
   def camelize(key) when is_atom(key) do
     key |> Atom.to_string |> camelize
--- test/caramelizer_test.exs
+++ test/caramelizer_test.exs
@@ -1,55 +1,13 @@
 defmodule CaramelizeTest do
   use ExUnit.Case, async: true
+  doctest Caramelize
 
   defstruct test_foo: "foo",
     test_bar: "bar",
     test_baz: "baz"
 
   describe "camelize/1" do
-    test "works with a string" do
-      assert Caramelize.camelize("cool_string") == "coolString"
-    end
 
-    test "works with an atom" do
-      assert Caramelize.camelize(:cool_string) == "coolString"
-    end
-
-    test "works with a flat map" do
-      assert Caramelize.camelize(%{cool_atom: "foo"}) == %{"coolAtom" => "foo"}
-      assert Caramelize.camelize(%{"cool_string" => "bar"}) == %{"coolString" => "bar"}
-    end
-
-    test "works with a nested map" do
-      assert Caramelize.camelize(%{cool_atom: %{another_cool_atom: "foo"}}) ==
-        %{"coolAtom" => %{"anotherCoolAtom" => "foo"}}
-      assert Caramelize.camelize(%{"cool_string" => %{"another_cool_string" => "bar"}}) ==
-        %{"coolString" => %{"anotherCoolString" => "bar"}}
-    end
-
-    test "works with a list" do
-      assert Caramelize.camelize([%{cool_key: "foo"}]) == [%{"coolKey" => "foo"}]
-    end
-
-    test "works with a list of maps" do
-        assert Caramelize.camelize(%{list_key: [%{foo: 12, bar: 13}]}) ==
-                             %{"listKey" => [%{"foo" => 12, "bar" => 13}]}
-    end
-
-    test "works with a DateTime struct" do
-      now = DateTime.utc_now
-      assert Caramelize.camelize(now) == %DateTime{calendar: now.calendar,
-                                                   day: now.day,
-                                                   hour: now.hour,
-                                                   microsecond: now.microsecond,
-                                                   minute: now.minute,
-                                                   month: now.month,
-                                                   second: now.second,
-                                                   std_offset: now.std_offset,
-                                                   time_zone: now.time_zone,
-                                                   utc_offset: now.utc_offset,
-                                                   year: now.year,
-                                                   zone_abbr: now.zone_abbr}
-    end
 
     test "works with an Ecto.DateTime struct" do
       {:ok, now} = Ecto.DateTime.cast("2016-05-21 12:12:12")
