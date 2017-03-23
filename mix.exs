dc7327af50ff86fcaeebf271a96bfe69c23dd7a8
James Rasmussen <james@podium.co>
2017-03-23 15:32:05 -0600

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Caramelize.Mixfile do
 
   def project do
     [app: :caramelize,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
