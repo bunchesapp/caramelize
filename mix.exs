0129302748015824156bacf7d48718e39ea75fdb
Nate Turner <nate.turner@podium.com>
2018-05-09 14:17:28 -0600

    bump ecto and elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Caramelize.Mixfile do
   def project do
     [app: :caramelize,
      version: "0.1.2",
-     elixir: "~> 1.3",
+     elixir: "~> 1.6.2",
      description: description(),
      package: package(),
      build_embedded: Mix.env == :prod,
@@ -33,7 +33,7 @@ defmodule Caramelize.Mixfile do
   defp deps do
     [{:ex_doc, ">= 0.0.0", only: :dev},
      {:credo, "~> 0.4", only: [:dev, :test]},
-     {:ecto, "~> 2.1.3"}]
+     {:ecto, "~> 2.2.0"}]
   end
 
   defp package do
